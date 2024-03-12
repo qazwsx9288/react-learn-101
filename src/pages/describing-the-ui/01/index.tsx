7e555f9f6e8d21c6509e5a5d00fd948654cdec21
wang2hao <378619897@qq.com>
2024-03-12 13:41:07 +0800

    fix: ui 01

--- src/pages/describing-the-ui/01/index.tsx
+++ src/pages/describing-the-ui/01/index.tsx
@@ -1,12 +1,10 @@
 // 页面标题
 export const pageTitle = 'Export the component 1/4'
 
-import { Profile } from './profile'
-
 export default function BasePage() {
   return (
     <>
-      <Profile></Profile>
+      <img src="https://i.imgur.com/lICfvbD.jpg" alt="Aklilu Lemma" />
     </>
   )
 }
