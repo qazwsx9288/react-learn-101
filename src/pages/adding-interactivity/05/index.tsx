30b04f45cfe079991031a520d0ed86e95de91beb
wang2hao <378619897@qq.com>
2024-03-15 16:55:05 +0800

    feat: interactivity 05 completed

---
+++ src/pages/adding-interactivity/05/index.tsx
@@ -0,0 +1,10 @@
+export const pageTitle = 'Remove unnecessary state'
+
+export default function FeedbackForm() {
+  function handleClick() {
+    const name = prompt('What is your name?')
+    alert(`Hello, ${name}!`)
+  }
+
+  return <button onClick={handleClick}>Greet</button>
+}
