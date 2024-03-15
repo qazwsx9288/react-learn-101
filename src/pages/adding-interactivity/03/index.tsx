6345f88531935a26379c9bec0fec6c4378ceec4e
wang2hao <378619897@qq.com>
2024-03-15 16:37:10 +0800

    feat: interactivity 03 completed

---
+++ src/pages/adding-interactivity/03/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+export const pageTitle = 'Fix stuck form inputs'
+
+export default function Form() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+
+  function handleFirstNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setFirstName(e.target.value)
+  }
+
+  function handleLastNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setLastName(e.target.value)
+  }
+
+  function handleReset() {
+    setFirstName('')
+    setLastName('')
+  }
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        placeholder="Last name"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>
+        Hi, {firstName} {lastName}
+      </h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
+  )
+}
